d22db2b38b9580865f8d31fba006699ae55f9178
Priyanshu7798 <124646393+Priyanshu7798@users.noreply.github.com>
2025-02-26 00:32:24 +0530

    Context

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import { AuthProvider } from './context/authContext'
+import { AuthProvider } from './context/AuthContexts'
 import { QueryProvider } from './lib/react-query/QueryProvider'
 
 createRoot(document.getElementById('root')!).render(
